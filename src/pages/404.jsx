12c3917caff0f8386213ad8c7e4be929b566d5ef
aMeijerr <caa.meijer@gmail.com>
2023-01-11 12:05:16 +0100

    Add 404 and brushed up css on projects

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,16 +1,47 @@
-import * as React from "react"
-import { Link } from "gatsby"
-import Layout from "../components/Layout"
+import * as React from 'react';
+import Layout from '../components/Layout';
+import { useStaticQuery, graphql } from 'gatsby';
+import { GatsbyImage, getImage } from 'gatsby-plugin-image';
+import * as styles from '../styles/404.module.css';
 
 const NotFoundPage = () => {
+  const query = useStaticQuery(graphql`
+    query PageNotFoundQuery {
+      allContentful404Page {
+        edges {
+          node {
+            title
+            text
+            image {
+              gatsbyImageData(
+                height: 350
+                resizingBehavior: PAD
+                placeholder: BLURRED
+                layout: FIXED
+              )
+            }
+          }
+        }
+      }
+    }
+  `);
+
+  const data = query.allContentful404Page.edges[0].node;
+  const img = getImage(data.image);
+  console.log(data);
   return (
     <Layout>
-      <h1>Page not found</h1>
-      <Link to="/">Go home</Link>.
+      <section className={styles.container}>
+        <GatsbyImage image={img} alt={data.title} />
+        <div className={styles.text}>
+          <h2>{data.title}</h2>
+          <h3>{data.text}</h3>
+        </div>
+      </section>
     </Layout>
-  )
-}
+  );
+};
 
-export default NotFoundPage
+export default NotFoundPage;
 
-export const Head = () => <title>Not found</title>
+export const Head = () => <title>Not found</title>;
--- src/pages/projects.jsx
+++ src/pages/projects.jsx
@@ -1,106 +1,115 @@
-import React from "react";
-import Layout from "../components/Layout";
-import { graphql, Link } from "gatsby";
-import * as styles from "../styles/projects.module.css";
-// import { BLOCKS, MARKS } from "@contentful/rich-text-types";
-// import { renderRichText } from "gatsby-source-contentful/rich-text";
-import { GatsbyImage, getImage } from "gatsby-plugin-image";
-// import Vue from "../assets/Vue.svg";
-// import Javascript from "../assets/JavaScript.svg";
-// import Python from "../assets/Angular.svg";
+import React from 'react';
+import Layout from '../components/Layout';
+import { graphql, Link } from 'gatsby';
+import * as styles from '../styles/projects.module.css';
+import { GatsbyImage, getImage } from 'gatsby-plugin-image';
+import { useMediaQuery } from 'react-responsive';
+import { DeviceSize } from '../components/responsive';
 
-// const options = {
-//   renderMark: {
-//     [MARKS.BOLD]: text => <strong>{text}</strong>,
-//   },
-//   renderNode: {
-//     [BLOCKS.PARAGRAPH]: (node, children) => <p>{children}</p>,
-//   },
-// }
+const ProjectsPage = ({ data }) => {
+  const isMobile = useMediaQuery({ maxWidth: DeviceSize.mobile });
+  let TechList = [];
+  return (
+    <Layout>
+      <section>
+        {data.allContentfulProjects.edges.forEach((edge) => {
+          const projectTech = edge.node.tech.title;
+          TechList.push(projectTech);
+        })}
+        {console.log(TechList[0])}
 
-const ProjectsPage = ({data}) => {
-  let TechList = []
-    return (
-        <Layout>
-            <section>
-            {data.allContentfulProjects.edges.forEach((edge) => {
-              const projectTech = edge.node.tech.title;
-                TechList.push(projectTech)
-            })}
-            {console.log(TechList[0])}
-        
-            {/* {TechList.map((tech, i) => {
-              console.log(tech, i)
-              return (
-                <div className={styles.tech}>
-                  <img src={tech} alt="css-icon" width="40" height="40"/>
+        <nav className={styles.tech__navbar}>
+          <Link to={'/projects/category/' + TechList[0]}>
+            <h3>Javascript</h3>
+          </Link>
+          <Link to={'/projects/category/' + TechList[1]}>
+            <h3>Python</h3>
+          </Link>
+          <Link to={'/projects/category/' + TechList[2]}>
+            <h3>Vue</h3>
+          </Link>
+        </nav>
+
+        {data.allContentfulProjects.edges.map(({ node }) => {
+          const slug = node.slug;
+          const projectImage = getImage(node.projectImages[2]);
+
+          return (
+            <>
+              {isMobile ? (
+                <div className={styles.projects__container} key={node.title}>
+                  <hr />
+                  <h2>{node.title}</h2>
+                  <p>{node.tech.title}</p>
+                  <p>{node.description}</p>
+                  <div className={styles.img__container}>
+                    <GatsbyImage image={projectImage} alt={node.title} />
+                  </div>
+                  <Link to={'/project/' + slug}>
+                    <h4>View project</h4>
+                  </Link>
                 </div>
-              )
-            }
-            )} */}
-            <nav className={styles.tech__navbar}>
-                  <Link to={"/projects/category/" + TechList[0]}><h4>Javascript</h4></Link>
-                  <Link to={"/projects/category/" + TechList[1]}><h4>Python</h4></Link>
-                  <Link to={"/projects/category/" + TechList[2]}><h4>Vue</h4></Link>
-            </nav>
-
-                {data.allContentfulProjects.edges.map(({ node }) => {
-                    const slug = node.slug;
-                    const techSlug = node.tech.title;
-                    console.log(techSlug)
-                    const projectImage = getImage(node.projectImages[2]);
-                    return (
-                        <div className={styles.projects__container} key={node.title}>
-                          {/* <div className={styles.tech}>
-                              <img src={TechList[0]} alt="css-icon" width="40" height="40"/>
-                          </div> */}
-                            <hr />
-                            <h2>{node.title}</h2>
-                            <p>{node.tech.title}</p>
-                            <p>{node.description}</p>
-                            <div className={styles.img__container}>
-                              <GatsbyImage image={projectImage} alt={node.title}/>
-                              {/* {node.projectImages.map((img, i) => {
-                              const image = getImage(img);
-                              return <GatsbyImage image={image} alt={node.title} key={i}/>
-                              })} */}
-                            </div>
-                            {/* <p>{renderRichText(node.link, options)}</p> */}
-                            <Link to={"/project/" + slug}><h4>View project</h4></Link>
-                        </div>
-                    )
-                })}
-            </section>
-        </Layout>
-    );
-    };
+              ) : (
+                <div className={styles.projects__container} key={node.title}>
+                  <hr />
+                  <div className={styles.container__desktop}>
+                    <div className={styles.img__container}>
+                      <GatsbyImage image={projectImage} alt={node.title} />
+                    </div>
+                    <div className={styles.content__desktop}>
+                      <h2>{node.title}</h2>
+                      <p>{node.tech.title}</p>
+                      <p>{node.description}</p>
+                      <Link to={'/project/' + slug}>
+                        <h4>View project</h4>
+                      </Link>
+                    </div>
+                  </div>
+                </div>
+              )}
+            </>
+            // <div className={styles.projects__container} key={node.title}>
+            //     <hr />
+            //     <h2>{node.title}</h2>
+            //     <p>{node.description}</p>
+            //     <div className={styles.img__container}>
+            //       <GatsbyImage image={projectImage} alt={node.title}/>
+            //     </div>
+            //     <Link to={"/project/" + slug}><h4>View project</h4></Link>
+            // </div>
+          );
+        })}
+      </section>
+    </Layout>
+  );
+};
 
 export default ProjectsPage;
 
 export const ProjectsPageQuery = graphql`
-query ProjectsQuery {
+  query ProjectsQuery {
     allContentfulProjects {
-        edges {
-          node {
+      edges {
+        node {
+          title
+          slug
+          description
+          projectImages {
+            gatsbyImageData(
+              height: 350
+              resizingBehavior: PAD
+              placeholder: BLURRED
+              layout: FIXED
+            )
+          }
+          link {
+            raw
+          }
+          tech {
             title
-            slug
-            description
-            projectImages {
-              gatsbyImageData(
-                height: 350
-                resizingBehavior: PAD
-                placeholder: BLURRED
-                layout: FIXED
-              )
-            }
-            link {
-              raw
-            }
-            tech {
-              title
-            }
           }
         }
       }
     }
-`
+  }
+`;
